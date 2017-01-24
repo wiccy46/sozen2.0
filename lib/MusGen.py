c26991daa729fcc224b9742a84d93fd3ae0d0234
jiajun yang <eric1986_chn@yahoo.com>
2017-01-24 15:46:03 +0100

    working on musgen function

--- lib/MusGen.py
+++ lib/MusGen.py
@@ -1,19 +1,19 @@
-import cv2
 import numpy as np
 import threading, sys, time
 
 class MusGen(threading.Thread):
-    def __init__(self, keyponts, zones):
+    def __init__(self, zones):
         threading.Thread.__init__(self)
-        self.kp = keyponts
         self.zones = zones
         self.baseNote = -1
         self._stop = threading.Event()
         self.notes = np.zeros(9)
 
+
     def run(self):
         # I should put the generative modal here.
         # Next step is try to play a sample note and see if it works. ..
+        print (self.zones)
         self.baseNote =  np.argmax(self.zones)
         self.notes[self.baseNote] = 0
         if (self.baseNote == 0):
@@ -23,20 +23,19 @@ class MusGen(threading.Thread):
                 else:
                     self.notes[i] = self.notes[i - 1] + 1
         elif (self.baseNote == 8):
-            for i in range (9, 0):
-                if i == 8:
-                    pass
-                else:
-                    self.note[i] = self.note[i + 1] - 1
+            for i in range (8):
+                self.notes[8 - i - 1] = self.notes[8 - i] - 1
+
         else:
-            for i in range(self.baseNote -1, 0):
-                self.notes[i] = self.notes[i + 1] - 1
-            for i in range(self.baseNote + 1, 8):
+            for i in range(0, self.baseNote):
+                self.notes[self.baseNote - i - 1] = self.notes[self.baseNote - i ] - 1
+            for i in range(self.baseNote + 1, 9):
                 self.notes[i] = self.notes[i - 1] + 1
         print (self.notes)
 
 
     def stopit(self):
+        print ("Stop it called. ")
         self._stop.set()
 
     def stopped(self):
--- main.py
+++ main.py
@@ -44,185 +44,175 @@ fontSize = 2
 
 
 class Capture():
-	def __init__(self):
-		global  calibration_pts
-		self.capturing = False
-		self.cameraChoice = 0
-		self.c = cv2.VideoCapture(cameraChoice)
-		self.textColor = 255
-		self.initBrightness = 40
-		self.calibration_pts = calibration_pts
-		self.threshold_black = 183
+    def __init__(self):
+        global  calibration_pts
+        self.capturing = False
+        self.cameraChoice = 0
+        self.c = cv2.VideoCapture(cameraChoice)
+        self.textColor = 255
+        self.initBrightness = 40
+        self.calibration_pts = calibration_pts
+        self.threshold_black = 183
 
-	def changeCamera(self, choice):
-		cameraChoice = choice
-		self.c = cv2.VideoCapture(cameraChoice)
+    def changeCamera(self, choice):
+        cameraChoice = choice
+        self.c = cv2.VideoCapture(cameraChoice)
 
-	def changeBt(self, val):
-		self.threshold_black = val
+    def changeBt(self, val):
+        self.threshold_black = val
 
-	def startCapture(self):
-		self.capturing = True
-		ret, original_frame = self.c.read()
-		original_frame = cv2.cvtColor(original_frame, cv2.COLOR_BGR2GRAY)
-		# Left and now is wrongly flip.
-		original_frame = cv2.flip(original_frame, 0)
-		original_frame = cv2.flip(original_frame, 1)
-		snap_thres = 8.0
-		previous_frame = np.array([])
-		self.just_snapped = False
-		self.snapshot_flag = False
+    def startCapture(self):
+        self.capturing = True
+        ret, original_frame = self.c.read()
+        original_frame = cv2.cvtColor(original_frame, cv2.COLOR_BGR2GRAY)
+        # Left and now is wrongly flip.
+        original_frame = cv2.flip(original_frame, 0)
+        original_frame = cv2.flip(original_frame, 1)
+        snap_thres = 8.0
+        previous_frame = np.array([])
+        self.just_snapped = False
+        self.snapshot_flag = False
 
-		while(self.capturing):
-			ret, frame = self.c.read()
-			frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-			# Left and now is wrongly flip.
-			frame = cv2.flip(frame, 0); frame = cv2.flip(frame, 1)
-			frame = calibrate(frame, self.calibration_pts)
+        while(self.capturing):
+            ret, frame = self.c.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Left and now is wrongly flip.
+            frame = cv2.flip(frame, 0); frame = cv2.flip(frame, 1)
+            frame = calibrate(frame, self.calibration_pts)
 
-			try:
-				diff = cv2.absdiff(frame, previous_frame)
-				mean_diff = float(np.mean(diff))
-				if self.just_snapped:
-					mean_diff = 3.0
-					self.just_snapped = False
+            try:
+                diff = cv2.absdiff(frame, previous_frame)
+                mean_diff = float(np.mean(diff))
+                if self.just_snapped:
+                    mean_diff = 3.0
+                    self.just_snapped = False
 
-				try:
-					if(self.snapshot_flag == False) & (mean_diff > snap_thres):
-						print ("Mean Diff: " + str(mean_diff))
-						self.snapshot_flag = True
-					elif(self.snapshot_flag == True) & (mean_diff < snap_thres):
-						# Take a snap shot
-						time.sleep(1.5)
-						ret, frame = self.c.read()
-						frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-						# Left and now is wrongly flip.
-						frame = cv2.flip(frame, 0); frame = cv2.flip(frame, 1)
-						frame = calibrate(frame, self.calibration_pts)
-						row,column = np.shape(frame)[0], np.shape(frame)[1]
+                try:
+                    if(self.snapshot_flag == False) & (mean_diff > snap_thres):
+                        print ("Mean Diff: " + str(mean_diff))
+                        self.snapshot_flag = True
+                    elif(self.snapshot_flag == True) & (mean_diff < snap_thres):
+                        # Take a snap shot
+                        time.sleep(1.5)
+                        ret, frame = self.c.read()
+                        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                        # Left and now is wrongly flip.
+                        frame = cv2.flip(frame, 0); frame = cv2.flip(frame, 1)
+                        frame = calibrate(frame, self.calibration_pts)
+                        row,column = np.shape(frame)[0], np.shape(frame)[1]
 
-						self.keypoints, self.black_blob, self.blob_zones = lib.Stones.blobDetection(frame,\
-												self.threshold_black,  row, column)
+                        self.keypoints, self.black_blob, self.blob_zones = lib.Stones.blobDetection(frame,\
+                                                self.threshold_black,  row, column)
 
-						self.music = MusGen(self.keypoints, self.blob_zones)
-						self.music.start()
-
-
+                        self.music = MusGen(self.blob_zones)
+                        self.music.start()
 
 
+                        # Extract blob coordinates
+                        self.bblob_coordinates = lib.Stones.findCoordinates(self.keypoints)
+                        # Return the diameter of the blob.
+                        self.bblob_sizes = lib.Stones.findSize(self.keypoints)
+                        # Draw circles for blob
+                        frame = cv2.drawKeypoints(frame, self.keypoints, np.array([]), (0, 255, 0),
+                                                cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                    # ---------------
+                        cv2.imshow('Results', cv2.resize(frame, None, \
+                            fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA))
+                        cv2.imshow('Black', cv2.resize(self.black_blob, None, \
+                            fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA))
+                        self.snapshot_flag = False
+                        self.just_snapped = True
 
+                    elif (self.snapshot_flag == True) & (mean_diff > snap_thres):
+                        pass
+                    else:
+                        pass
+                except UnboundLocalError:
+                    pass
+            except ValueError:
+                pass
+            except cv2.error:
+                pass
+            previous_frame = frame.copy()
+            cv2.waitKey(400)
 
-						# Extract blob coordinates
-						self.bblob_coordinates = lib.Stones.findCoordinates(self.keypoints)
-						# Return the diameter of the blob.
-						self.bblob_sizes = lib.Stones.findSize(self.keypoints)
-						# Draw circles for blob
-						frame = cv2.drawKeypoints(frame, self.keypoints, np.array([]), (0, 255, 0),
-												cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-					# ---------------
+    def endCapture(self):
+        print ("end")
+        self.capturing = False
+        self.music.stopit()
 
-						cv2.imshow('Results', cv2.resize(frame, None, \
-							fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA))
-						cv2.imshow('Black', cv2.resize(self.black_blob, None, \
-							fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA))
-						self.snapshot_flag = False
-						self.just_snapped = True
-
-					elif (self.snapshot_flag == True) & (mean_diff > snap_thres):
-						pass
-					else:
-						pass
-				except UnboundLocalError:
-					pass
-			except ValueError:
-				pass
-			except cv2.error:
-				pass
-			previous_frame = frame.copy()
-			cv2.waitKey(400)
-
-	def endCapture(self):
-		print ("end")
-		self.capturing = False
-
-	def quitCapture(self):
-		print ("quit")
-		cap = self.c
-		self.capturing = False
-		cv2.destroyAllWindows()
-		cap.release()
-		QtCore.QCoreApplication.quit()
+    def quitCapture(self):
+        cap = self.c
+        self.capturing = False
+        cv2.destroyAllWindows()
+        self.music.stopit()
+        cap.release()
+        QtCore.QCoreApplication.quit()
 
 class Window(QtGui.QWidget):
-	def __init__(self):
-		super(Window, self).__init__()
-		self.capture = Capture()
-		self.setWindowTitle('SoZen v2.0')
+    def __init__(self):
+        super(Window, self).__init__()
+        self.capture = Capture()
+        self.setWindowTitle('SoZen v2.0')
 
-		self.camera_choice_box = QtGui.QSpinBox()
-		self.camera_choice_box.setValue(0)
-		self.camera_choice_box.setRange(0, 4)
-		self.camera_choice_box.setFixedWidth(60)
-		self.camera_choice_box.setToolTip("Restart the camera after changing")
-		self.camera_choice_box.valueChanged[int].connect(self.changeValue)
-		self.camera_choice_laybel = QtGui.QLabel('Camera')
-		self.camera_choice_laybel.setFixedSize(50, 20)
-
-
+        self.camera_choice_box = QtGui.QSpinBox()
+        self.camera_choice_box.setValue(0)
+        self.camera_choice_box.setRange(0, 4)
+        self.camera_choice_box.setFixedWidth(60)
+        self.camera_choice_box.setToolTip("Restart the camera after changing")
+        self.camera_choice_box.valueChanged[int].connect(self.changeValue)
+        self.camera_choice_laybel = QtGui.QLabel('Camera')
+        self.camera_choice_laybel.setFixedSize(50, 20)
 
 
-		self.start_button = QtGui.QPushButton('Start', self)
-		self.start_button.clicked.connect(self.capture.startCapture)
+        self.start_button = QtGui.QPushButton('Start', self)
+        self.start_button.clicked.connect(self.capture.startCapture)
 
-		self.end_button = QtGui.QPushButton('End', self)
-		self.end_button.clicked.connect(self.capture.endCapture)
+        self.end_button = QtGui.QPushButton('End', self)
+        self.end_button.clicked.connect(self.capture.endCapture)
 
-		self.quit_button = QtGui.QPushButton('Quit', self)
-		self.quit_button.clicked.connect(self.capture.quitCapture)
+        self.quit_button = QtGui.QPushButton('Quit', self)
+        self.quit_button.clicked.connect(self.capture.quitCapture)
 
-		lbox = QtGui.QGridLayout(self)
-		lbox.addWidget(self.start_button, 1,0, 1,1)
-		lbox.addWidget(self.end_button, 2, 0, 2, 1)
-		lbox.addWidget(self.quit_button, 3, 0, 3, 1)
-		lbox.addWidget(self.camera_choice_laybel, 5, 0)
-		lbox.addWidget(self.camera_choice_box, 5, 1)
-
-
-		# self.bt_laybel = QtGui.QLabel('Blob Threshold')
-		# self.bt_slider = QtGui.QSlider(QtCore.Qt.Horizontal, self)
-		# self.bt_slider.setRange(0, 255)
-		# self.bt_slider.setValue(183) # Need to change here.
-		# self.bt_slider.valueChanged[int].connect(self.changeValue)
+        lbox = QtGui.QGridLayout(self)
+        lbox.addWidget(self.start_button, 1,0, 1,1)
+        lbox.addWidget(self.end_button, 2, 0, 2, 1)
+        lbox.addWidget(self.quit_button, 3, 0, 3, 1)
+        lbox.addWidget(self.camera_choice_laybel, 5, 0)
+        lbox.addWidget(self.camera_choice_box, 5, 1)
+        # self.bt_laybel = QtGui.QLabel('Blob Threshold')
+        # self.bt_slider = QtGui.QSlider(QtCore.Qt.Horizontal, self)
+        # self.bt_slider.setRange(0, 255)
+        # self.bt_slider.setValue(183) # Need to change here.
+        # self.bt_slider.valueChanged[int].connect(self.changeValue)
         #
         #
-		# rbox = QtGui.QGridLayout(self)
-		# rbox.addWidget(self.bt_laybel, 1, 0)
-		# rbox.addWidget(self.bt_slider,1,1)
+        # rbox = QtGui.QGridLayout(self)
+        # rbox.addWidget(self.bt_laybel, 1, 0)
+        # rbox.addWidget(self.bt_slider,1,1)
         #
         #
-		# box = QtGui.QHBoxLayout(self)
-		# box.addLayout(lbox)
-		# box.addLayout(rbox)
-		self.setLayout(lbox)
-		self.setGeometry(100, 100, 400, 400)
-		self.show()
+        # box = QtGui.QHBoxLayout(self)
+        # box.addLayout(lbox)
+        # box.addLayout(rbox)
+        self.setLayout(lbox)
+        self.setGeometry(100, 100, 400, 400)
+        self.show()
 
-	def changeValue(self, value):
-		if self.sender() == self.camera_choice_box:
-			self.capture.changeCamera(value)
+    def changeValue(self, value):
+        if self.sender() == self.camera_choice_box:
+            self.capture.changeCamera(value)
 
-		elif self.sender() == self.bt_slider:
-			self.capture.changeBt(value)
-
-
+        elif self.sender() == self.bt_slider:
+            self.capture.changeBt(value)
 
 # The first step is to get the calibration coordinates
 calibration_pts = getCalibrationCoordinates()
 
 def main():
-	app = QtGui.QApplication(sys.argv)
-	sozen = Window()
-	sys.exit(app.exec_())
+    app = QtGui.QApplication(sys.argv)
+    sozen = Window()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-	main()
+    main()
