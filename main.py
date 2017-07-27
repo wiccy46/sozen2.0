62fa2de058d9f08423f6935132e96a49a4541e96
akhiljain100 <akhiljain100@gmail.com>
2017-07-27 11:21:37 +0200

    Check gitignore

--- main.py
+++ main.py
@@ -91,8 +91,6 @@ class Capture():
             # Left and now is wrongly flip.
             frame = cv2.flip(frame, 0); frame = cv2.flip(frame, 1)
             frame = calibrate(frame, self.calibration_pts)
-
-
             try:
                 diff = cv2.absdiff(frame, previous_frame)
                 mean_diff = float(np.mean(diff))
