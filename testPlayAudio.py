27124cc4655368417b64a2063c1e6cfb139783b0
wiccy46 <eric1986_chn@yahoo.com>
2017-07-29 18:59:17 +0200

    pygame play need time.sleep.

--- testPlayAudio.py
+++ testPlayAudio.py
@@ -1,7 +1,6 @@
-import pygame, os
-
+import pygame, os, time
 os.chdir('./audio/sine') # load image. 
 pygame.mixer.init()
 pygame.mixer.music.load("n3.wav")
 pygame.mixer.music.play()
-print "finished Play"
+time.sleep(2) # This is important
